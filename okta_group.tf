ddc5442e63f01bc42bbbdd69efa3133d778dc12b
parthswadas <42744567+parthswadas@users.noreply.github.com>
2021-06-10 18:12:12 +0530

    Update okta_group.tf

--- okta_group.tf
+++ okta_group.tf
@@ -1,3 +1,13 @@
+variable "org_name" {}
+variable "api_token" {}
+variable "base_url" {}
+
+provider "okta" {
+  org_name = var.org_name
+  base_url = var.base_url
+  api_token = var.api_token
+} 
+
 resource "okta_group" "test" {
   name        = "testAcc"
   description = "testing, testing"
